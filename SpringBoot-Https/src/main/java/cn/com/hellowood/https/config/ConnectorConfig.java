268686e7c3d74b93749f2af84c2a33e3f0ed33a6
HelloWood <hellowoodes@outlook.com>
2018-12-31 23:06:09 +0800

    Add https of springboot

---
+++ SpringBoot-Https/src/main/java/cn/com/hellowood/https/config/ConnectorConfig.java
@@ -0,0 +1,52 @@
+package cn.com.hellowood.https.config;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author HelloWood
+ * @date 2018-12-31 22:04
+ */
+@Configuration
+public class ConnectorConfig {
+
+    @Value("${server.port.http}")
+    private int serverPortHttp;
+
+    @Value("${server.port}")
+    private int serverPortHttps;
+
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection securityCollection = new SecurityCollection();
+                securityCollection.addPattern("/*");
+                securityConstraint.addCollection(securityCollection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        factory.addAdditionalTomcatConnectors(redirectConnector());
+        return factory;
+    }
+
+    private Connector redirectConnector() {
+        Connector connector = new Connector(Http11NioProtocol.class.getName());
+        connector.setScheme("http");
+        connector.setPort(serverPortHttp);
+        connector.setSecure(false);
+        connector.setRedirectPort(serverPortHttps);
+        return connector;
+    }
+}
